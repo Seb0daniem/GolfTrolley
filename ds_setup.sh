14021475bff9a487125cddfae5595ad23027a4c7
daniel <damor005@hotmail.com>
2025-11-15 17:59:41 +0100

    Working mqtt

--- ds_setup.sh
+++ ds_setup.sh
@@ -5,6 +5,7 @@ mkdir /opt/nvidia/deepstream/deepstream-7.1/samples/models/Tracker
 # Copy config files
 cp /GolfTrolley/configfiles/config_infer_primary.txt /opt/nvidia/deepstream/deepstream-7.1/samples/configs/deepstream-app/
 cp /GolfTrolley/configfiles/source1_usb_dec_infer_resnet_int8.txt /opt/nvidia/deepstream/deepstream-7.1/samples/configs/deepstream-app/
+cp /GolfTrolley/configfiles/mqtt_config.txt /opt/nvidia/deepstream/deepstream-7.1/samples/configs/deepstream-app/
 
 # Copy models
 cp /GolfTrolley/models/resnet18_trafficcamnet_pruned.onnx_b1_gpu0_int8.engine /opt/nvidia/deepstream/deepstream-7.1/samples/models/Primary_Detector
